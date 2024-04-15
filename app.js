7d9b194ea1f9bf79219519bb79f337e92ce9bf44
Jarib Cockerel <jarib.cockerel@outlook.com>
2024-04-14 21:07:46 -0500

    logged answer to refactor ES2015

--- app.js
+++ app.js
@@ -6,3 +6,11 @@ function double(arr) {
     });
 }
 */
+
+//ES2015 version
+
+//arr is the object
+//map is the array method we to use
+//end result is to have each value passed thru to be multiplied by 2
+
+const double = (arr) => arr.map((val) => val * 2);
